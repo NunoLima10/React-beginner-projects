255431a60d802e74080379ee4d5a4058cd90579d
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-03-31 22:00:19 -0100

    feat: form discipline

---
+++ src/Components/AddNewDiscipline.jsx
@@ -0,0 +1,12 @@
+import FormDiscipline from "./FormDiscipline"
+
+
+const AddNewDiscipline = () => {
+  return (
+    <div>
+        <FormDiscipline/>
+    </div>
+  )
+}
+
+export default AddNewDiscipline
